0e9354ec1737959d82547fa2650f707db2d233ca
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2015-05-28 20:53:39 +0200

    Correcting account to access use in userInfo.

--- index.js
+++ index.js
@@ -1080,7 +1080,7 @@ OpenIDConnect.prototype.userInfo = function() {
     var self = this;
     return [
             self.check('openid', /profile|email/),
-            self.use({policies: {loggedIn: false}, models: ['account', 'user']}),
+            self.use({policies: {loggedIn: false}, models: ['access', 'user']}),
             function(req, res, next) {
                 req.model.access.findOne({token: req.parsedParams.access_token})
                 .exec(function(err, access) {
